2423aaa95b39f7c412ea820a10f982d292fdd7bd
vivek mandavara <vivekmandavara1029@gmail.com>
2026-09-02 17:41:52 +0530

    how to create a simple array using arrays concept.
     for example ; given input -
    enter the size of array = n.
    given that n element of array.
    now, 
    the array is created .

---
+++ create array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int n,i;
+	cout<<"enter the size of the array = ";
+	cin>>n;
+	//cout<<"create array using any name";
+	//cout<<"for example: i have given that arr[].";
+	int arr[n];
+	//cout<<"array start from 0th index to (n-1)th index";
+	for(i=0;i<n;i++){
+	cout<<"enter the value of  "<<i+1<<"st element of array = ";
+	cin>>arr[i];}
+		return 0;}
